fa2d1824a0ff1c4e288c0109511228478d76bfac
Montgomery Gray <montgomerygray55@gmail.com>
2025-06-23 14:57:53 -0400

    add basic_usage.py

---
+++ basic_usage.py
@@ -0,0 +1,16 @@
+import numpy as np
+import scipy
+from src.integrator import integrate_1d
+
+f = lambda x: np.exp(-x**2)
+a, b = 0, 1
+exact = 0.5*(np.pi)**(0.5)*scipy.special.erf(1)
+trap = np.round(integrate_1d(f, a, b, method="trapezoidal", n=100), 6)
+sim = np.round(integrate_1d(f, a, b, method="simpson", n=100), 6)
+gauss = np.round(integrate_1d(f, a, b, method="gauss", n=100), 6)
+
+print("Method, value, error" )
+print(f"Trapezoidal: {trap}, {np.round(exact - trap, 6)}")
+print(f"Simpson:{sim}, {np.round(exact - sim, 6)}")
+print(f"Gauss-Legendre:{gauss}, {np.round(exact - gauss, 6)}")
+
