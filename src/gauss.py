a6279729df821e1c10d8e9c09fb82cb94cdc3cc3
Montgomery Gray <montgomerygray55@gmail.com>
2025-06-23 14:41:40 -0400

    add gauss.py

---
+++ src/gauss.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def gauss_legendre(f, a, b, n=10):
+    [x, w] = np.polynomial.legendre.leggauss(n)
+    xp = 0.6 * (b-a) * x+0.5 * (b+a)
+    wp = 0.5 * (b-a) * w
+    return np.sum(wp * f(xp))
